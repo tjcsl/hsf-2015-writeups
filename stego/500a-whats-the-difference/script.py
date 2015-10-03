2599037a07861df03bedd5d511127064446e8204
Fox Wilson <foxwilson123@gmail.com>
2015-10-03 00:18:16 -0400

    simply What's The Difference script

--- stego/500a-whats-the-difference/script.py
+++ stego/500a-whats-the-difference/script.py
@@ -6,15 +6,10 @@ i2 = Image.open("DF93D5.png").load()
 i3 = Image.new("RGB", (1831, 2000), (255, 255, 255))
 i3p = i3.load()
 
-things = {}
 for x in range(1831):
     for y in range(2000):
         z = tuple(map(operator.mul, (16, 16, 16, 0), tuple(map(operator.sub, i1[x, y], i2[x, y]))))
         if z[0] or z[1] or z[2] or z[3]:
-            if z not in things:
-                things[z] = 0
-            things[z] += 1
             i3p[x, y] = 0,0,0
-print(things)
 
 i3.save("result.png")
